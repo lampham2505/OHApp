68d1ca40a343b110eb3588fc57ceb34eae91deb4
Viet Anh <anhviet1704@gmail.com>
2016-11-10 13:33:15 +0700

    new branch

--- OneHealthApp/View/AppDelegate.swift
+++ OneHealthApp/View/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate,CLLocationManagerDelegate 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         //viet commit
+        //new branch
         return true
     }
 
