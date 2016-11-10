325d9e3e6a3c9bab1dd84d501c48db8c583f91a9
Viet Anh <anhviet1704@gmail.com>
2016-11-10 13:24:30 +0700

    test

--- OneHealthApp/View/AppDelegate.swift
+++ OneHealthApp/View/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate,CLLocationManagerDelegate 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //viet commit
         return true
     }
 
