d5336e5bf582e1f0a58239971f0b053744af4da0
Viet Anh <anhviet1704@gmail.com>
2016-11-16 08:29:52 +0700

    commit Viet

--- OneHealthApp/View/ServiceViewController/Login:Register/LoginViewController.swift
+++ OneHealthApp/View/ServiceViewController/Login:Register/LoginViewController.swift
@@ -50,20 +50,11 @@ class LoginViewController: UIViewController {
                 print(json)
                 if let response = res as? HTTPURLResponse , 200...299 ~= response.statusCode {
                 } else {
+                    
                 }
             }
         }
         dataTask.resume()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
